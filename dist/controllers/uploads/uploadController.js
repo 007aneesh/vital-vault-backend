42280dc733aac8af688f5a593b00aba549c68071
007aneesh <aneeshx000@gmail.com>
2025-03-07 01:18:09 +0530

    fix: upload controller

--- dist/controllers/uploads/uploadController.js
+++ dist/controllers/uploads/uploadController.js
@@ -15,38 +15,73 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const uploadService_1 = __importDefault(require("../../services/uploads/uploadService"));
 const handle_response_1 = require("../../utils/handle_response");
 const singleton_class_1 = require("../../utils/singleton_class");
+const patient_services_1 = __importDefault(require("../../services/patient-services/patient_services"));
+const appAssert_1 = __importDefault(require("../../utils/appAssert"));
+const http_1 = require("../../utils/http");
+const db_1 = require("../../utils/db");
+const employee_services_1 = __importDefault(require("../../services/employee-services/employee_services"));
+const organisation_services_1 = __importDefault(require("../../services/settings/organisation_services"));
 class UploadController {
     constructor() {
-        this.handleUpload = this.handleUpload.bind(this);
-        this.uploadOrganization = this.uploadOrganization.bind(this);
-        this.uploadReports = this.uploadReports.bind(this);
-        this.uploadVerifiedDocs = this.uploadVerifiedDocs.bind(this);
-        this.uploadProfileImage = this.uploadProfileImage.bind(this);
-    }
-    handleUpload(req, res, category) {
-        return __awaiter(this, void 0, void 0, function* () {
+        this.handleUpload = (req, category) => __awaiter(this, void 0, void 0, function* () {
             var _a;
             if (!req.processedFiles) {
-                return (0, handle_response_1.sendError)(res, "No files provided for upload", 400);
+                throw new Error("No files provided for upload");
             }
             if (!((_a = req.payload) === null || _a === void 0 ? void 0 : _a.user_id)) {
-                return (0, handle_response_1.sendError)(res, "User authentication required", 401);
+                throw new Error("User authentication required");
             }
-            const results = yield uploadService_1.default.uploadSingleFile(req.processedFiles, category, req.payload.user_id);
+            try {
+                return yield uploadService_1.default.uploadSingleFile(req.processedFiles, category, req.payload.user_id);
+            }
+            catch (error) {
+                console.error("Error in handleUpload:", error);
+                throw new Error("Internal server error");
+            }
+        });
+        this.uploadOrganization = (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const results = yield this.handleUpload(req, "organization");
             return (0, handle_response_1.sendSuccess)(res, "File uploaded successfully", 200, results);
         });
-    }
-    uploadOrganization(req, res) {
-        return this.handleUpload(req, res, "organization");
-    }
-    uploadReports(req, res) {
-        return this.handleUpload(req, res, "reports");
-    }
-    uploadVerifiedDocs(req, res) {
-        return this.handleUpload(req, res, "verified_docs");
-    }
-    uploadProfileImage(req, res) {
-        return this.handleUpload(req, res, "profile_images");
+        this.uploadReports = (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const results = yield this.handleUpload(req, "reports");
+            return (0, handle_response_1.sendSuccess)(res, "File uploaded successfully", 200, results);
+        });
+        this.uploadVerifiedDocs = (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const results = yield this.handleUpload(req, "verified_docs");
+            return (0, handle_response_1.sendSuccess)(res, "File uploaded successfully", 200, results);
+        });
+        this.uploadProfileImage = (req, res) => __awaiter(this, void 0, void 0, function* () {
+            const response = yield this.handleUpload(req, "verified_docs");
+            if (!response) {
+                return (0, handle_response_1.sendError)(res, "Error uploading profile image", 400);
+            }
+            const updatedData = {
+                image: response.url,
+            };
+            const user = yield db_1.prisma.entity_Mapping.findUnique({
+                where: { ref_id: req.payload.user_id },
+                include: {
+                    organisation: true,
+                    employee: true,
+                    patient: true,
+                },
+            });
+            if (!user)
+                return (0, handle_response_1.sendError)(res, "User not found", 404);
+            let result = null;
+            if (user.type === "organisation" /* UserType.ORGANISATION */ && user.organisation) {
+                result = yield organisation_services_1.default.updateOrganisation(req.payload.user_id, updatedData);
+            }
+            else if (user.type === "employee" /* UserType.EMPLOYEE */ && user.employee) {
+                result = yield employee_services_1.default.updateDetails(req.payload.user_id, updatedData);
+            }
+            else if (user.type === "patient" /* UserType.PATIENT */ && user.patient) {
+                result = yield patient_services_1.default.updatePatient(req.payload.user_id, updatedData);
+            }
+            (0, appAssert_1.default)(result, http_1.INTERNAL_SERVER_ERROR, "Error uploading image", "File upload failed" /* AppErrorCode.FileUploadFailed */);
+            return (0, handle_response_1.sendSuccess)(res, "Profile image uploaded successfully", 200, response);
+        });
     }
 }
 const methods = (0, singleton_class_1.SingletonClass)(UploadController);
--- src/controllers/uploads/uploadController.ts
+++ src/controllers/uploads/uploadController.ts
@@ -2,48 +2,101 @@
 import uploadService from "../../services/uploads/uploadService";
 import { sendError, sendSuccess } from "../../utils/handle_response";
 import { SingletonClass } from "../../utils/singleton_class";
+import patient_services from "../../services/patient-services/patient_services";
+import appAssert from "../../utils/appAssert";
+import { INTERNAL_SERVER_ERROR } from "../../utils/http";
+import AppErrorCode from "../../utils/appErrorCode";
+import { prisma } from "../../utils/db";
+import { UserType } from "../../utils/constant";
+import employee_services from "../../services/employee-services/employee_services";
+import organisation_services from "../../services/settings/organisation_services";
 
 class UploadController {
-  constructor() {
-    this.handleUpload = this.handleUpload.bind(this);
-    this.uploadOrganization = this.uploadOrganization.bind(this);
-    this.uploadReports = this.uploadReports.bind(this);
-    this.uploadVerifiedDocs = this.uploadVerifiedDocs.bind(this);
-    this.uploadProfileImage = this.uploadProfileImage.bind(this);
-  }
-
-  async handleUpload(req: any, res: Response, category: string) {
+  handleUpload = async (req: any, category: string) => {
     if (!req.processedFiles) {
-      return sendError(res, "No files provided for upload", 400);
+      throw new Error("No files provided for upload");
     }
     if (!req.payload?.user_id) {
-      return sendError(res, "User authentication required", 401);
+      throw new Error("User authentication required");
     }
 
-    const results = await uploadService.uploadSingleFile(
-      req.processedFiles,
-      category,
-      req.payload.user_id,
-    );
+    try {
+      return await uploadService.uploadSingleFile(
+        req.processedFiles,
+        category,
+        req.payload.user_id,
+      );
+    } catch (error) {
+      console.error("Error in handleUpload:", error);
+      throw new Error("Internal server error");
+    }
+  };
 
+  uploadOrganization = async (req: Request, res: Response) => {
+    const results = await this.handleUpload(req, "organization");
     return sendSuccess(res, "File uploaded successfully", 200, results);
-  }
+  };
 
-  uploadOrganization(req: Request, res: Response) {
-    return this.handleUpload(req, res, "organization");
-  }
+  uploadReports = async (req: Request, res: Response) => {
+    const results = await this.handleUpload(req, "reports");
+    return sendSuccess(res, "File uploaded successfully", 200, results);
+  };
 
-  uploadReports(req: Request, res: Response) {
-    return this.handleUpload(req, res, "reports");
-  }
+  uploadVerifiedDocs = async (req: Request, res: Response) => {
+    const results = await this.handleUpload(req, "verified_docs");
+    return sendSuccess(res, "File uploaded successfully", 200, results);
+  };
 
-  uploadVerifiedDocs(req: Request, res: Response) {
-    return this.handleUpload(req, res, "verified_docs");
-  }
+  uploadProfileImage = async (req: any, res: Response) => {
+    const response = await this.handleUpload(req, "verified_docs");
+    if (!response) {
+      return sendError(res, "Error uploading profile image", 400);
+    }
+    const updatedData = {
+      image: response.url,
+    };
 
-  uploadProfileImage(req: any, res: Response) {
-    return this.handleUpload(req, res, "profile_images");
-  }
+    const user = await prisma.entity_Mapping.findUnique({
+      where: { ref_id: req.payload.user_id },
+      include: {
+        organisation: true,
+        employee: true,
+        patient: true,
+      },
+    });
+
+    if (!user) return sendError(res, "User not found", 404);
+
+    let result = null;
+    if (user.type === UserType.ORGANISATION && user.organisation) {
+      result = await organisation_services.updateOrganisation(
+        req.payload.user_id,
+        updatedData,
+      );
+    } else if (user.type === UserType.EMPLOYEE && user.employee) {
+      result = await employee_services.updateDetails(
+        req.payload.user_id,
+        updatedData,
+      );
+    } else if (user.type === UserType.PATIENT && user.patient) {
+      result = await patient_services.updatePatient(
+        req.payload.user_id,
+        updatedData,
+      );
+    }
+    appAssert(
+      result,
+      INTERNAL_SERVER_ERROR,
+      "Error uploading image",
+      AppErrorCode.FileUploadFailed,
+    );
+    return sendSuccess(
+      res,
+      "Profile image uploaded successfully",
+      200,
+      response,
+    );
+  };
 }
 
 const methods = SingletonClass(UploadController);
