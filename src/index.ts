2ae07e63b0220972b3bb3a4219ac760cb0b26e20
Aneesh Aggarwal <90706436+007aneesh@users.noreply.github.com>
2026-02-16 13:03:43 +0530

    fix: add frontend base to cors

--- src/index.ts
+++ src/index.ts
@@ -25,7 +25,7 @@ app.use(
 app.use(express.json());
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000", "https://vital-vault-plus.vercel.app"],
     credentials: true,
   }),
 );
