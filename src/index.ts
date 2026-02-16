d8bf751eef8232de77f5427ad3f2cddc1ef8d4ff
Aneesh Aggarwal <90706436+007aneesh@users.noreply.github.com>
2026-02-16 13:25:05 +0530

    fix: test

--- src/index.ts
+++ src/index.ts
@@ -20,6 +20,8 @@ app.use(
   cors({
     origin: process.env.FRONTEND_ORIGIN,
     credentials: true,
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   }),
 );
 
